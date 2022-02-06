1f803fdb7ca66a5317c8cbf1868bb234ad1c821a
unnamed <elverabo@su-d3.msk.21-school.ru>
2022-02-06 12:46:26 +0300

    first

--- src/drawing.c
+++ src/drawing.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-#define
-#define
+#define FIELD_WIDTH 80
+#define FIELD_HEIGHT 25
+#define RACKET_WIDTH 3
+#define RACKET_SHIFT 3
 int x = 80;
 int y = 0;
-void draw(ball_x, ball_y, racket1, racket2)
+void draw(ball_x, ball_y, racket1, racket2, score1, score2)
     {
 while (y != 26 ) {
     y = y + 1;
-    for(int i = 0; x >= i; i++) {
+    for(int i = 0; FIELD_WIDTH >= i; i++) {
         if((y == 1) || (y == 26)) {
             printf("-");
         } else {
@@ -16,7 +18,7 @@ while (y != 26 ) {
                 printf("|");
             } else {
                 if ((y == 2) && (i == 40)) {
-                    printf("%d||%d", 0, 0);
+                    printf("%d||%d", score1, score2);
                     i+=4;
                 }
                 for(int j = 0; j < 3; j++) {
